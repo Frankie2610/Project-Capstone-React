24b5959d24510f512e30275db9db12dfb8af77a0
Văn Gia Quyền <giaquyenvan@gmail.com>
2023-12-01 17:23:07 +0700

    adjust token

--- src/apis/axiosClient.js
+++ src/apis/axiosClient.js
@@ -5,7 +5,7 @@ const axiosClient = axios.create({
   baseURL: "https://movienew.cybersoft.edu.vn/api",
   headers: {
     TokenCybersoft:
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCA0MiIsIkhldEhhblN0cmluZyI6IjMwLzA5LzIwMjMiLCJIZXRIYW5UaW1lIjoiMTY5NjAzMjAwMDAwMCIsIm5iZiI6MTY2NzA2MjgwMCwiZXhwIjoxNjk2MTc5NjAwfQ.i6JqYnGkwyHl6dkDHnjFWbPfBEl2l4SXAp4r7h9Ecpw",
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCA0MiIsIkhldEhhblN0cmluZyI6IjExLzEyLzIwMjMiLCJIZXRIYW5UaW1lIjoiMTcwMjI1MjgwMDAwMCIsIm5iZiI6MTY2NzA2MjgwMCwiZXhwIjoxNzAyNDAwNDAwfQ.WRTj_6tr0SSgX4a_yoloJ5KyL5DGv9E21DyHyblms2A",
   },
 });
 
